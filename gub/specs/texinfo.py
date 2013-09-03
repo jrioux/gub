e5c667b528fe00f70e04b2d032d8e4c724bda9a5
Julien Rioux <julien.rioux@gmail.com>
2013-09-03 15:34:56 +0200

    Patch texinfo to be compatible with dash.
    dash (and possibly other shells) refuses to override builtins.

--- gub/specs/texinfo.py
+++ gub/specs/texinfo.py
@@ -2,6 +2,7 @@ from gub import tools
 
 class Texinfo__tools (tools.AutoBuild):
     source = 'http://ftp.gnu.org/pub/gnu/texinfo/texinfo-4.13a.tar.gz'
+    patches = ['texinfo-4.13-dash.patch']
     def patch (self):
         tools.AutoBuild.patch (self)
         # Drop ncurses dependency
