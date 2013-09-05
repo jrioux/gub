8838a55180d1fae72a4bea90a952993af8bfaf29
Julien Rioux <julien.rioux@gmail.com>
2013-09-05 15:58:55 +0200

    Update psmisc from version 22.2 to 22.7
    Version 22.2 is no longer available.
    
    Also fix the psmisc compilation:
    Avoid 'undefined reference to rpl_malloc' errors.

--- gub/specs/psmisc.py
+++ gub/specs/psmisc.py
@@ -1,6 +1,10 @@
 from gub import target
 
 class Psmisc (target.AutoBuild):
-    source = 'http://surfnet.dl.sourceforge.net/sourceforge/psmisc/psmisc-22.2.tar.gz'
+    source = 'http://downloads.sourceforge.net/project/psmisc/psmisc/Archive/psmisc-22.7.tar.gz'
     subpackage_names = ['']
     dependencies = ['ncurses']
+    config_cache_overrides = target.AutoBuild.config_cache_overrides + '''
+ac_cv_func_malloc_0_nonnull=yes
+ac_cv_func_realloc_0_nonnull=yes
+'''
