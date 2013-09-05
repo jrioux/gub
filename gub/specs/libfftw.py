34d90b8047a16a5019202fe5be3feb5de1cd65cb
Julien Rioux <julien.rioux@gmail.com>
2013-09-05 15:54:38 +0200

    Minor update of libfftw from 3.2.1 to 3.2.2
    Version 3.2.1 is no longer available.

--- gub/specs/libfftw.py
+++ gub/specs/libfftw.py
@@ -1,5 +1,5 @@
 from gub import target
 
 class Libfftw (target.AutoBuild):
-    source = 'http://www.fftw.org/fftw-3.2.1.tar.gz'
+    source = 'http://www.fftw.org/fftw-3.2.2.tar.gz'
     dependencies = ['tools::automake', 'tools::libtool', 'tools::pkg-config',]
