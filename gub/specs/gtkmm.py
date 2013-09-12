b5161342118c9038959acc9c0349892c8218e2ef
Julien Rioux <julien.rioux@gmail.com>
2013-09-12 17:57:36 +0200

    Minor update of gtkmm from 2.14.3 to 2.14.4
    Fixes the following error when building (GCC 4.4.x):
    recentinfo.cc:192: error: invalid conversion from 'gchar**' to 'const gchar**'

--- gub/specs/gtkmm.py
+++ gub/specs/gtkmm.py
@@ -1,5 +1,5 @@
 from gub import target
 
 class Gtkmm (target.AutoBuild):
-    source = 'http://ftp.gnome.org/pub/GNOME/sources/gtkmm/2.14/gtkmm-2.14.3.tar.gz'
+    source = 'http://ftp.gnome.org/pub/GNOME/sources/gtkmm/2.14/gtkmm-2.14.4.tar.gz'
     dependencies = ['cairomm-devel', 'gtk+-devel', 'pangomm-devel']
